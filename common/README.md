3ef0325d6fd7283a3753472c485141a5d8013637
Denis Sologub <contedevel2010@gmail.com>
2018-11-16 15:53:31 +0300

    Adds log library

--- common/README.md
+++ common/README.md
@@ -2,6 +2,11 @@
 
 This package contains common shared libraries for SMTP-server and client.
 
+## Build
+
+Use command `make`, all libraries will be created in `../dist/libs`.
+ 
 ## Libraries
+
 - libcore
 - liblog
---
+++ common/log/log.c
@@ -0,0 +1,23 @@
+#include "log.h"
+
+#include <stdio.h>
+
+void log_d(const char *msg) {
+    printf("%s", msg);
+}
+
+void log_e(const char *msg) {
+    printf("%s", msg);
+}
+
+void log_f(const char *msg) {
+    printf("%s", msg);
+}
+
+void log_i(const char *msg) {
+    printf("%s", msg);
+}
+
+void log_w(const char *msg) {
+    printf("%s", msg);
+}
