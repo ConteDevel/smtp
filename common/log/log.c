e64e6013617f57fa734f19d1ce2237a2cc2b9a71
Denis Sologub <contedevel2010@gmail.com>
2018-11-17 01:42:02 +0300

    Adds log samples

--- common/log/log.c
+++ common/log/log.c
@@ -2,22 +2,30 @@
 
 #include <stdio.h>
 
+void log_stdout(const char lvl, const char *msg) {
+	printf("%c: %s\n", lvl, msg);
+}
+
+void log_stderr(const char lvl, const char *msg) {
+	printf("%c: %s\n", lvl, msg);
+}
+
 void log_d(const char *msg) {
-    printf("%s", msg);
+    log_stdout('D', msg);
 }
 
 void log_e(const char *msg) {
-    printf("%s", msg);
+    log_stderr('E', msg);
 }
 
 void log_f(const char *msg) {
-    printf("%s", msg);
+    log_stderr('F', msg);
 }
 
 void log_i(const char *msg) {
-    printf("%s", msg);
+    log_stdout('I', msg);
 }
 
 void log_w(const char *msg) {
-    printf("%s", msg);
+    log_stdout('W', msg);
 }
---
+++ common/log/main.c
@@ -0,0 +1,11 @@
+#include "log.h"
+
+int main() {
+    log_i("Information");
+    log_d("Debugging");
+    log_w("Warning");
+    log_e("Error");
+    log_f("Wasted");
+
+    return 0;
+}
